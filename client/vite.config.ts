0aa541f65fcca35bef9f4594ee4177ce34be3be4
jnmnltrnz <jnmnltrnz@gmail.com>
2025-05-04 21:38:46 +0800

    Deploy Vite app to GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/URL_Shortener/', // Set this to your GitHub repository name
   server: {
     port: 3000,
     host: true,
   },
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure output is directed to the 'dist' folder
+  },
 });
